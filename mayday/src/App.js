9f434e89229f6a81e2148d28ef0fda85b8434ef4
jonstults <jdstults1@gmail.com>
2018-11-10 14:17:04 -0600

    fuck you guys

--- mayday/src/App.js
+++ mayday/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src="https://www.blossomcostumes.com.au/media/catalog/product/cache/1/image/1800x/040ec09b1e35df139433887a97daa66f/i/n/inflatable-penis-costume-w.jpg" className="App-logo" alt="logo" />
           <img src="https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/522604_10100576233630188_1264083236_n.jpg?_nc_cat=107&_nc_ht=scontent-ort2-2.xx&oh=890e0cc7165a1e0f56ce9b9e272f3a9f&oe=5C8A4E12" className="App-logo" alt="logo" />
           <p>
-            Jon <code>is/A.dick</code> and a menance to society.
+            Jon <code>is/A.hero</code> and a menance to society.
           </p>
           <a
             className="App-link"
